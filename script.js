f50a2f41dbf5c94602d12272cd5980acc5873a37
montelzek <patrykmatras378@gmail.com>
2024-09-06 18:25:14 +0200

    Added function for checking win and for chrcking tie.

--- script.js
+++ script.js
@@ -84,51 +84,7 @@ const gameController = (function() {
 
 gameController.init();
 
-function makeMove(player, row, column) {
-    if (board[row][column] === ' ') {
-        board[row][column] = player;
-        return true;
-    } else {
-        console.log('Invalid move, try gain!');
-        return false;
-    }
-}
 
-function checkWin(player) {
-    for (let i = 0; i < 3; i++) {
-        if (board[i].every(cell => cell === player)) return true;
-        if (board.map(row => row[i]).every(cell => cell === player)) return true;
-    }
-    if (board[0][0] === player && board[1][1] === player && board[2][2] === player) return true;
-    if (board[0][2] === player && board[1][1] === player && board[2][0] === player) return true;
-    return false;
-}
 
-function checkDraw() {
-    return board.flat().every(cell => cell !== ' ');
-}
 
-function playGame() {
-    let currentPlayer = 'X';
-    let turns = 0;
-    while (true) {
-        printBoard();
-        let row = prompt(`Player ${currentPlayer}, enter your move row (0 - 2): `);
-        let col = prompt(`Player ${currentPlayer}, enter your move column (0 - 2): `);
-
-        if (makeMove(currentPlayer, row, col)) {
-            turns++;
-            if (checkWin(currentPlayer)) {
-                printBoard();
-                console.log(`Player ${currentPlayer} wins!`);
-                break;
-            } else if (checkDraw()) {
-                printBoard();
-                console.log("It's a draw!");
-                break;
-            }
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        }
-    }
-}
 
